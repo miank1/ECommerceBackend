1d93e3408a932b681e28e620b682523005192471
Ankit Mishra <ankit1703@gmail.com>
2026-06-15 13:36:03 +0530

    events

--- pkg/events/checkout_requested.go
+++ pkg/events/checkout_requested.go
@@ -1,5 +1,15 @@
 package events
 
 type CheckoutRequested struct {
-	UserID string `json:"user_id"`
+	UserID     string         `json:"user_id"`
+	Items      []CheckoutItem `json:"items"`
+	TotalPrice float64        `json:"total_price"`
+}
+
+type CheckoutItem struct {
+	ProductID   string  `json:"product_id"`
+	ProductName string  `json:"product_name"`
+	Category    string  `json:"category"`
+	Quantity    int     `json:"quantity"`
+	Price       float64 `json:"price"`
 }
